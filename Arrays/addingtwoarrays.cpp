b600ee9b13d492c79f057c6b89c3ce5b198e7858
Ujjwal Bhatt <93533269+Ujjwalbhatt@users.noreply.github.com>
2022-09-16 17:33:07 +0530

    master

---
+++ Arrays/addingtwoarrays.cpp
@@ -0,0 +1,66 @@
+
+#include <bits/stdc++.h> 
+using namespace std;
+vector<int> reverse(vector<int> &b)
+{
+    int n = 0;
+    int m = b.size() - 1;
+    while (n < m)
+    {
+        swap(b[n++], b[m--]);
+    }
+    return b;
+}
+vector<int> findArraySum(vector<int> &a, int n, vector<int> &b, int m)
+{
+    int i = n - 1;
+    int j = m - 1;
+    int carry = 0;
+    vector<int> ans;
+    while (i >= 0 && j >= 0)
+    {
+        int val1 = a[i];
+        int val2 = b[j];
+        int sum = val1 + val2 + carry;
+        carry = sum / 10;
+        sum = sum % 10;
+        ans.push_back(sum);
+        i--;
+        j--;
+    }
+    while (i >= 0)
+    {
+        int sum = a[i] + carry;
+        carry = sum / 10;
+        sum = sum % 10;
+        ans.push_back(sum);
+        i--;
+    }
+    while (j >= 0)
+    {
+        int sum = b[j] + carry;
+        carry = sum / 10;
+        sum = sum % 10;
+        ans.push_back(sum);
+        j--;
+    }
+    while (carry != 0)
+    {
+        int sum = b[j] + carry;
+        carry = sum / 10;
+        sum = sum % 10;
+        ans.push_back(sum);
+    }
+    return reverse(ans);
+}
+int main()
+{
+    vector<int> a = {1, 2, 3, 4};
+    vector<int> b = {9, 8, 7, 6};
+    vector<int> ans = findArraySum(a, 4, b, 4);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
+    return 0;
+}
